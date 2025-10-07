450656d523767e0c5080e11cecb2e84d6433982a
Mikka Akdag <mia011@edu.zealand.dk>
2025-10-07 10:58:48 +0200

    lavet Task 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 //Write your Javascript code here
+
+// Task 1
+let findliste = document.getElementById("list")
+let nytelement = document.createElement("li")
+
+nytelement.setAttribute("id", "4")
+nytelement.setAttribute("class", "unhealthy")
+
+let elementtekst = document.createTextNode("Marabou")
+nytelement.appendChild(elementtekst)
+
+findliste.appendChild(nytelement)
+
+
 console.log("Shoppinglist")
